f46905ade94805c6d883f4ad32199c5158604858
Seok Won Kang <kanggo1111@gmail.com>
2024-02-11 13:37:01 +0900

    add spinkit splash screen for DB load
    Details:
    add spinkit splash screen for DB load
    
    TODO:
    make simple to add data
    i

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:hair_designer_sales_manage/my_widget/my_calendar/my_calendar.dart';
+import 'package:hair_designer_sales_manage/items/my_db.dart';
 import 'package:hair_designer_sales_manage/screens/sub_srceen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  bool _isLoaded = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loading();
+  }
+
+  void _loading() async{
+    myDB = MyDB();
+    await myDB.readMyDB();
+    setState(() {
+      _isLoaded = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +39,9 @@ class MainScreen extends StatelessWidget {
           title: Text('Main Screen'),
           elevation: 0,
         ),
-        body: SubScreen()
-
-        // Center(
-        //   child: MyCalendar(),
-        // ),
+        body: _isLoaded ? SubScreen() : Center(
+          child: SpinKitRing(color: Colors.indigo,),
+        )
       ),
     );
   }
